f3d8f1dda3bf741b251046683813113fe01cf18b
Whrgo <79298870+whrgo@users.noreply.github.com>
2022-07-31 14:44:28 +0900

    Work2

--- src/components/result.js
+++ src/components/result.js
@@ -1,39 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Result({ finded }) {
   const [result, setResult] = useState([]);
   const [isResult, setIsResult] = useState(false);
 
-  function readTextFile(file) {
-    let rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function () {
-      if (rawFile.readyState === 4) {
-        if (rawFile.status === 200 || rawFile.status == 0) {
-          let allText = rawFile.responseText;
+  const [kklong, setKklong] = useState("");
+  const [isKklong, setIsKklong] = useState(false);
+
+  const [kkutu, setKkutu] = useState("");
+  const [isKkutu, setIsKkutu] = useState(false);
+
+  // read txt file at react
+  const readFile = async (filename) => {
+    const response = await fetch("https://whgro.github.io/src/" + filename);
+    const data = await response.text();
+    return data;
+  };
+
+  useEffect(() => {
+    setKklong(readFile("kklong.txt"));
+    setKklong((prev) => prev.split("\n"));
+    setIsKklong(true);
+    setKkutu(readFile("kkutu.txt"));
+    setKklong((prev) => prev.split("\n"));
+    setIsKkutu(true);
+  }, []);
+
+  useEffect(() => {
+    if (finded === true) {
+      setIsResult(true);
+    } else {
+      setIsResult(false);
+    }
+  }, [finded]);
+
+  const find_reuslt = () => {
+    if (isKklong && isKkutu) {
+      const result = [];
+      for (let i = 0; i < kklong.length; i++) {
+        for (let j = 0; j < kkutu.length; j++) {
+          if (kklong[i] === kkutu[j]) {
+            result.push(kklong[i]);
+          }
         }
       }
-    };
-    rawFile.send(null);
-  }
-
-  readTextFile("../data/kklong.txt");
+      setResult(result);
+      setIsResult(true);
+    }
+  };
 
   return (
     <div className="result">
-      {finded ? (
-        <>
-          {isResult ? (
-            {
-              /* Result */
-            }
-          ) : (
-            <h3>검색결과 없음</h3>
-          )}
-        </>
-      ) : (
-        <></>
-      )}
+      {finded ? <>{isResult ? {} : <h3>검색결과 없음</h3>}</> : <></>}
     </div>
   );
 }
